3a21df7aaf4e5eb9ede8867df5017082bb70da03
foxesaremid99 <145400849+foxesaremid99@users.noreply.github.com>
2023-11-29 09:59:13 -0500

    Update js.js

--- js/js.js
+++ js/js.js
@@ -200,14 +200,14 @@ function openMenu(...x) {
   }
 }
 function inspect() {
-  (function () {
-    var script = document.createElement("script");
-    script.src = "js/inspect.js";
-    script.className = "webxray";
-    script.setAttribute("data-lang", "en-US");
-    script.setAttribute("data-baseuri", "https://x-ray-goggles.mouse.org");
+  (function() {
+    var script = document.createElement('script');
+    script.src = 'https://x-ray-goggles.mouse.org/webxray.js';
+    script.className = 'webxray';
+    script.setAttribute('data-lang', 'en-US');
+    script.setAttribute('data-baseuri', 'https://x-ray-goggles.mouse.org');
     document.body.appendChild(script);
-  })();
+}())
 }
 function opencity(a) {
   tc = document.getElementsByClassName("iframething");
